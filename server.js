a0d859abd43e6b03aa1bb2d4d1c2f9023bbd431e
Vinay1249 <pvinaykumar2209@gmail.com>
2024-12-18 00:02:01 +0530

    second commit

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/',(request,response)=>{
-	response.send('welcome to my automated application')
+	response.send('welcome to the world of waste')
 })
 
 app.listen(4000, '0.0.0.0' ,()=>{
 	console.log('server started at port 4000')
 })
-
 
